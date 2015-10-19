7294036367ae8afafdbd622635b0a96ccc7ece5d
itculture <github@it-culture.com>
2015-10-19 15:46:17 +0200

    Addes some Classes

---
+++ InnovationToolkit/InnovationToolkit/Topics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _365MethodeClassic
+{
+    class Topics
+    {
+        private int topicId { set; get; }
+        private string topicSummary { set; get; }
+        private string topicDescription { set; get; }
+
+        Topics(int _topicId, string _topicSummary, string _topicDescription)
+        {
+            this.topicId = _topicId;
+            this.topicSummary = _topicSummary;
+            this.topicDescription = _topicDescription;
+
+        }
+    }
+}
